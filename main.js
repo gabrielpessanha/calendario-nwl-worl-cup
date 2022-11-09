a4586b5666f81869f6a45920904c2c7f800905a6
gabriel pessanha <gabrielpessanha777@gmail.com>
2022-11-09 05:12:44 -0300

    testing

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ function createGame(icon, hour, icon2){
  return `
   <li>
 <a href="https://www.google.com/search?q=seleção+${icon}">
-<img src="/assets/${icon}.JPG" alt="bandeira ${icon}"/>
+<img src="./assets/${icon}.svg" alt="bandeira ${icon}"/>
 </a>
         <strong>${hour}</strong>
 <a href="https://www.google.com/search?q=seleção+${icon2}">
-<img src="/assets/icon=${icon2}.svg" alt="bandeira ${icon2}"/>
+<img src="./assets/icon=${icon2}.svg" alt="bandeira ${icon2}"/>
 </a>
    </li>
  ` 
